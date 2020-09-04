03e37c44b2ec48036a82dc006063bf629e63bd14
sxc <qiuyequ@icloud.com>
2020-09-04 09:27:40 +0800

    Initial Commit

---
+++ ShapeSettingsDemo/ShapeSettingsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeSettingsDemoApp.swift
+//  ShapeSettingsDemo
+//
+//  Created by Xiaochun Shen on 2020/9/4.
+//
+
+import SwiftUI
+
+@main
+struct ShapeSettingsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
